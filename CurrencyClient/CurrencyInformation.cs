d3aa46284ad0237edc7ab220edd8e338e2c42ae9
Souri <Sourilog@gmail.com>
2016-05-15 14:20:26 +0100

    YourCurrencyClient
    YourCurrencyClient

---
+++ CurrencyClient/CurrencyInformation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Currency.Models
+{
+    // Currency info i.e. Name, Rate Currency, Date ExChange
+    public class CurrencyInformation
+    {
+        
+        public String Currencyname
+        {
+            get;
+            set;
+        }
+
+       
+        public double CurrencyRate
+        {
+            get;
+            set;
+        }
+
+        public DateTime RateDate
+        {
+            get;
+            set;
+        }
+
+       
+        public bool CurrencyWarning { get; set; }
+    }
+}
